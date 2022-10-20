4635318af9a66adc4e8b8fc4cf860ece2890918e
SarunWu <96522041+SarunWu@users.noreply.github.com>
2022-10-20 23:13:02 +0800

    Summit assignment

---
+++ helpers/data_mining_helpers.py
@@ -0,0 +1,150 @@
+import re
+import matplotlib.pyplot as plt
+
+import gensim
+import nltk
+import spacy
+import pandas as pd
+from gensim.utils import simple_preprocess
+from spacy.lang.en.stop_words import STOP_WORDS
+import itertools
+import numpy as np
+
+"""
+Helper functions for data mining lab session 2018 Fall Semester
+Author: Elvis Saravia
+Email: ellfae@gmail.com
+"""
+
+
+def format_rows(docs):
+    """ format the text field and strip special characters """
+    D = []
+    for d in docs.data:
+        temp_d = " ".join(d.split("\n")).strip('\n\t')
+        D.append([temp_d])
+    return D
+
+
+def format_labels(target, docs):
+    """ format the labels """
+    return docs.target_names[target]
+
+
+def check_missing_values(row):
+    """ functions that check and verifies if there are missing values in dataframe """
+    counter = 0
+    for element in row:
+        if element == True:
+            counter += 1
+    return ("The amoung of missing records is: ", counter)
+
+
+def tokenize_text(text, remove_stopwords=False):
+    """
+    Tokenize text using the nltk library
+    """
+    tokens = []
+    for d in nltk.sent_tokenize(text, language='english'):
+        for word in nltk.word_tokenize(d, language='english'):
+            # filters here
+            tokens.append(word)
+    return tokens
+
+
+def format_topics_sentences(ldamodel, corpus, texts):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(
+                    pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Percentage_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return sent_topics_df
+
+
+def sent_to_words(sentences):
+    for sent in sentences:
+        sent = re.sub('\S*@\S*\s?', '', sent)  # remove emails
+        sent = re.sub('\s+', ' ', sent)  # remove newline chars
+        sent = re.sub("\'", "", sent)  # remove single quotes
+        sent = gensim.utils.simple_preprocess(str(sent), deacc=True)
+        yield (sent)
+
+
+def process_words(texts, stop_words=STOP_WORDS, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    bigram = gensim.models.Phrases(texts, min_count=5, threshold=100)  # higher threshold fewer phrases.
+    trigram = gensim.models.Phrases(bigram[texts], threshold=100)
+    bigram_mod = gensim.models.phrases.Phraser(bigram)
+    trigram_mod = gensim.models.phrases.Phraser(trigram)
+
+    """Remove Stopwords, Form Bigrams, Trigrams and Lemmatization"""
+    texts = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+    texts = [bigram_mod[doc] for doc in texts]
+    texts = [trigram_mod[bigram_mod[doc]] for doc in texts]
+    texts_out = []
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    # remove stopwords once more after lemmatization
+    texts_out = [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts_out]
+    return texts_out
+
+
+# Sentence Coloring of N Sentences
+def topics_per_document(model, corpus, start=0, end=1):
+    corpus_sel = corpus[start:end]
+    dominant_topics = []
+    topic_percentages = []
+    for i, corp in enumerate(corpus_sel):
+        topic_percs, wordid_topics, wordid_phivalues = model[corp]
+        dominant_topic = sorted(topic_percs, key=lambda x: x[1], reverse=True)[0][0]
+        dominant_topics.append((i, dominant_topic))
+        topic_percentages.append(topic_percs)
+    return (dominant_topics, topic_percentages)
+
+
+#
+
+
+def plot_confusion_matrix(cm, lables,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(lables))
+    plt.xticks(tick_marks, lables, rotation=0)
+    plt.yticks(tick_marks, lables)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+#%%
